f478d355fcebf1dc57ecf7091e7db2c8586c7302
Ameer Deen <writeameer@gmail.com>
2013-12-26 18:33:07 +1100

    Added Get VolumeDetail

---
+++ DiskPart/Models/VolumeDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace Cloudoman.DiskTools.Models
+{
+    public class VolumeDetail
+    {
+        public Disk Disk { get; set; }
+        public bool ReadOnly { get; set; }
+        public bool Hidden { get; set; }
+        public bool NoDefaultDriveLetter { get; set; }
+        public bool ShadowCopy { get; set; }
+        public bool Offline { get; set; }
+        public bool BitLockerEncrypted { get; set; }
+        public bool Installable { get; set; }
+        public string VolumeCapacity { get; set; }
+        public string VolumeFreeSpace { get; set; }
+    }
+}
